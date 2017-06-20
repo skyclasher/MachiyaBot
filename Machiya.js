5a0e05ac886e8e47c05fd5cd5b37fbac2d7dd38b
Ahmad Syafiq <ahmad.syafiq@gmail.com>
2017-06-21 00:58:21 +0800

    Commit

--- Machiya.js
+++ Machiya.js
@@ -2,36 +2,69 @@ const fs = require("fs")
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const Constants = require('./ConstantsHelper');
+
+
+var $ = require('jquery');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+//$.support.cors = true;
+$.ajaxSettings.xhr = function () {
+    return new XMLHttpRequest();
+};
 
 client.login(config.token);
 
 client.on("ready", () => {
-  console.log('Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.');
+    console.log("Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.");
 });
 
 client.on("message", (message) => {
-  // Set the prefix
-  let prefix = config.prefix;
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+    // Set the prefix
+    let prefix = config.prefix;
+    let validCommand = false;
 
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
-  }
-  
-	if(message.content.startsWith(prefix + "prefix")) {
-		// Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
-		let newPrefix = message.content.split(" ").slice(1, 2)[0];
-		// change the configuration in memory
-		prefix = newPrefix;
-		config.prefix = prefix;
-		// Now we have to save the file.
-		fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
-	}
-	
-	//if(message.author.id !== config.ownerID) return; for owner id
-  
+    // Exit and stop if the prefix is not there or if user is a bot
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    if (hasWhiteSpace(message.content)) {
+
+        if(message.content.startsWith(prefix + "test")) {
+            validCommand = true;
+
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5d340a588774b863f69e21741c34a9bc&tags=miyawaki+sakura&format=json&nojsoncallback=1&auth_token=72157685292708415-f8b508c8a2c8486f&api_sig=aee63d1ef5d2a76e9a72f6adc9dd0738",
+                "method": "GET",
+                "headers": {}
+            }
+            
+            $.ajax(settings).done(function (data) {
+                message.channel.send(data);
+            });
+
+        }
+
+        if (message.author.id === config.ownerID) {
+            if (message.content.startsWith(prefix + "prefix")) {
+                validCommand = true;
+                let newPrefix = message.content.split(" ").slice(1, 2)[0];
+                prefix = newPrefix;
+                config.prefix = prefix;
+                fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+            }
+        }
+
+        if (!validCommand)
+            message.channel.send(Constants.Error.CommandNotFound);
+
+    } else
+        message.channel.send(Constants.Error.InvalidCommand);
+
+
 });
+
+function hasWhiteSpace(s) {
+    return s.indexOf(' ') >= 0;
+}
